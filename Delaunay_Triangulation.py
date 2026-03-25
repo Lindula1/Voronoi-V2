29a0bcd56acc5fab80bb8615dea04bf4f0bab8d4
LindulaG <lgunarathne6@gmail.com>
2026-03-25 17:43:48 +1100

    centroid formula

--- Delaunay_Triangulation.py
+++ Delaunay_Triangulation.py
@@ -1,6 +1,10 @@
+import random
+
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+from numpy import ndarray
+
 
 class DelaunayV2:
     def __init__(self, points):
@@ -12,8 +16,11 @@ class DelaunayV2:
         normalised_points = self.normalize_points(points)
         bin_numbers = self.generate_bins_nums_2d(normalised_points)
         sorted_points = self.sort_bins(points, bin_numbers)
-        return sorted_points
+        print(sorted_points)
+        self.binned_points = self.bin_points(points, bin_numbers)
+        print(self.binned_points)
 
+        return sorted_points
 
     def normalize_points(self, points):
         """
@@ -25,19 +32,33 @@ class DelaunayV2:
         res = (points - np.min(points, axis=0)[np.newaxis,:])/max_points[np.newaxis,:]
         return res
 
+    def bin_points(self, points, bins):
+        N_max = np.max(bins)
+        binned_points = [[] for _ in range(N_max)]
+        for ind, bin_item in enumerate(bins):
+            # np.append(binned_points[bin_item-1], points[ind])
+            binned_points[bin_item-1].append(points[ind])
+
+        for j, f in enumerate(binned_points):
+            binned_points[j] = np.array(f)
+
+        return binned_points
+
     def generate_bins_nums_2d(self, points):
-        N = points.shape[0]
+        N = points.shape[0] # point number
         n = int(np.sqrt(N))
         bin_indices = ((0.99*n*points)/np.max(points, axis=0)[np.newaxis, :]).astype(int)
 
         bins = np.zeros(N, dtype=int)
         for i in range(N):
             bins[i] = bin_indices[i,1] * n + bin_indices[i,0] + 1 if bin_indices[i,1] % 2 == 0 else (bin_indices[i,1] + 1)* n - bin_indices[i,0]
+
         return bins
 
     def sort_bins(self, points, bin_numbers):
         sorted_points = knuth_counting_sort_numpy(points, bin_numbers)
         return sorted_points
+
 
 def knuth_counting_sort_numpy(coords: np.ndarray, ranks: np.ndarray) -> np.ndarray:
     """
@@ -82,13 +103,19 @@ def knuth_counting_sort_numpy(coords: np.ndarray, ranks: np.ndarray) -> np.ndarr
 
 
 if __name__ == "__main__":
-    np.random.seed(0)
-    seed_points = np.random.uniform(0, 10, (13,2))
+    np.random.seed(69)
+    random.seed(69)
+    seed_point_num = 14
+    seed_points = np.random.uniform(0, 10, (seed_point_num,2))
+    colours = [(random.random(), random.random(), random.random()) for i in range(seed_point_num)]
     # print(seed_points)
     delaunay = DelaunayV2(seed_points)
     triangulated = delaunay.triangulate()
+    binned_points = delaunay.binned_points
     plt.figure(1, figsize=(8,8))
-    plt.plot(seed_points, "o")
-    plt.figure(2, figsize=(8,8))
-    plt.plot(triangulated, "o")
+    for k in range(len(binned_points)):
+        plt.plot(binned_points[k][:,0], binned_points[k][:,1], "o", color=colours[k])
+    # plt.plot(seed_points[:,0], seed_points[:,1], "o")
+    # plt.figure(2, figsize=(8,8))
+    # plt.plot(triangulated[:,0], triangulated[:,1], "o")
     plt.show()
--- Polygon_Formulae.py
+++ Polygon_Formulae.py
@@ -1,12 +1,26 @@
 import numpy as np
 from matplotlib.patches import Polygon
 import matplotlib.pyplot as plt
+import matplotlib.patches as patch
 
 def poly_signed_area_2d(vertices):
     x = vertices[:, 0]
     y = vertices[:, 1]
     res = x[:-1] * y[1:] - x[1:] * y[:-1]
     return np.abs(np.sum(res))*0.5
+
+def gen_circum_center(vertices):
+    a, b, c = vertices
+    d = 2*(a[0]*(b[1]-c[1])+b[0]*(c[1]-a[1])+c[0]*(a[1]-b[1]))
+    u_x = (1/d)*(np.sum(a**2)*(b[1]-c[1])+np.sum(b**2)*(c[1]-a[1])+np.sum(c**2)*(a[1]-b[1]))
+    u_y = (1/d)*(np.sum(a**2)*(c[0]-b[0])+np.sum(b**2)*(a[0]-c[0])+np.sum(c**2)*(b[0]-a[0]))
+    return u_x, u_y
+
+def centroid_distance(vertices, centroid):
+    a, b = centroid
+    x, y = vertices[0][0], vertices[0][1]
+    distance = np.sqrt((x-a)**2+(y-b)**2)
+    return distance
 
 def poly_centroid_2d(vertices, signed_area):
 
@@ -28,25 +42,33 @@ if __name__ == "__main__":
     # polygon = np.random.uniform(0, 2,(7,2))
     # polygon = np.array([
     #     [0, 0],
-    #     [4, 0],
-    #     [4, 4],
+    #     [5, 0],
+    #     [4, 6],
     #     [0, 4],
     #     [0, 0]
     # ])
-    polygon = poly_gen.generate_polygon(center=(250, 250),
-                                avg_radius=100,
+    polygon = poly_gen.generate_polygon(center=(0, 0),
+                                avg_radius=20,
                                 irregularity=0.35,
                                 spikiness=0.2,
-                                num_vertices=16)
+                                num_vertices=3)
     polygon = np.array(polygon)
-    area = poly_signed_area_2d(polygon)
-    centroid = poly_centroid_2d(polygon, area)
+    # area = poly_signed_area_2d(polygon)
+    # centroid = poly_centroid_2d(polygon, area)
+    # radius = centroid_distance(polygon, centroid)
+    # print(centroid)
+    # polygon = np.array([[3,2],[1,4],[5,4]])
+    centroid = gen_circum_center(polygon)
     print(centroid)
+    radius = centroid_distance(polygon, centroid)
+
+    circle = patch.Circle(centroid, radius, color='r', fill=False)
 
     fig, ax = plt.subplots(1,1)
     p = Polygon(polygon, closed=True)
+    ax.add_patch(circle)
     ax.add_patch(p)
-    ax.plot(centroid[0], centroid[1], "go")
-    plt.ylim([0, 400])
-    plt.xlim([0, 400])
+    ax.plot(centroid[0], centroid[1], "ro")
+    plt.ylim([-50, 50])
+    plt.xlim([-50, 50])
     plt.show()
